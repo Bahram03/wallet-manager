4011f28cc0b16fea61157720ed313f87962f5a29
Mammad-Smoke <mohammadbahrami82@outlook.com>
2022-09-20 19:11:16 +0430

    feat: add models

--- app/models.py
+++ app/models.py
@@ -3,3 +3,31 @@ from app import db
 
 
 # TODO: Implement
+class Wallet(db.Model):
+    __tablename__ = 'wallets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False, index=True, unique=True)
+    created_at = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    coins = db.relationship('WalletCoin', cascade='all, delete')
+
+
+class Coin(db.Model):
+    __tablename__ = 'coins'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False, index=True, unique=True)
+    symbol = db.Column(db.String(255), nullable=False, index=True, unique=True)
+    price = db.Column(db.Numeric, nullable=False)
+    created_at = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    wallets = db.relationship('WalletCoin', cascade='all, delete')
+
+
+class WalletCoin(db.Model):
+    __tablename__ = 'wallet_coin'
+    id = db.Column(db.Integer, primary_key=True)
+    wallet_id = db.Column(db.ForeignKey('wallets.id'))
+    coin_id = db.Column(db.ForeignKey('coins.id'))
+    quantity = db.Column(db.Numeric, nullable=False)
+    coin = db.relationship('Coin', back_populates='wallets')
+    wallet = db.relationship('Wallet', back_populates='coins')
