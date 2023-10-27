8d044c21d9ba81cf49fcaa0fa9b8cf901cbedc04
Bahram <95754900+Bahram03@users.noreply.github.com>
2023-10-27 19:05:27 +0330

    Update errors.py

--- app/errors.py
+++ app/errors.py
@@ -1,6 +1,11 @@
 from flask import jsonify
 from app import app
 
+
 @app.errorhandler(404)
 def not_found_error(error):
     return jsonify({'error': 'not found'}), 404
+
+
+def no_name_found_error():
+    return jsonify({'error': 'no name provided'}), 400
